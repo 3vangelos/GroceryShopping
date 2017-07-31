b128d1f10c64f3f60a0be2881cb5bfc07360ee3d
Evangelos Sismanidis <evangelos@posteo.eu>
2017-07-31 20:07:25 +0200

    New Single View XCode Project created

---
+++ GroceryShopping/GroceryShopping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GroceryShopping
+//
+//  Created by Sismanidis, Evangelos (415) on 31.07.17.
+//  Copyright © 2017 Private. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
