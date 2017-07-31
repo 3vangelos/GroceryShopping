1234ab48aba3a1307d47d24bfa231412f956f112
Evangelos Sismanidis <evangelos@posteo.eu>
2017-07-31 21:52:41 +0200

    Initial Commit of UITableViewController

---
+++ GroceryShopping/GroceryShopping/Controllers/GroceriesViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  GroceryShopping
+//
+//  Created by Sismanidis, Evangelos (415) on 31.07.17.
+//  Copyright © 2017 Private. All rights reserved.
+//
+
+import UIKit
+
+class GroceriesViewController: UIViewController {
+
+    private let tableView = UITableView()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        self.view = tableView
+        self.title = "Grocery Shopping"
+    }
+}
+
+extension GroceriesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
